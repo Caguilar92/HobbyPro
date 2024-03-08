0dae9153b67c60ff81524fd2fec02b2c6bb302af
Candelario <candelarioa42@gmail.com>
2024-03-08 16:37:58 -0600

    "created registration form with named fields"

--- HobbyPro/src/views/RegisterView.vue
+++ HobbyPro/src/views/RegisterView.vue
@@ -1,9 +1,65 @@
 <script setup>
+import {ref} from "vue";
+import {getDatabase,ref as dbref,set} from "firebase/database";
 
+let firstname= ref('');
+let lastname= ref('');
+let email= ref('');
+let password= ref('');
+const database = getDatabase();
+function register(event) {
+  event.preventDefault();
+
+  set(dbref(database,'users/'), {
+    firstname: firstname.value,
+    lastname: lastname.value,
+    email: email.value,
+    password: password.value
+  })
+
+}
+
+function validate(data) {
+  return data !== null && data.length !== 0  && data.trim() !== 0
+}
 </script>
 
 <template>
-<h1>Register View</h1>
+
+  <h1 class="mt-5 text-center">Hobby Pro</h1>
+  <div class="justify-content-around d-flex mt-5">
+    <form class="border border-lg p-5">
+      <h4 class="text-center">Log In</h4>
+      <div class="d-flex">
+        <div class="mb-3 me-2">
+          <label  for="firstname" class="form-label">First Name</label>
+          <input v-model="firstname" type="text" class="form-control" id="firstname">
+        </div>
+        <div class="mb-3">
+          <label for="lastname" class="form-label">Last Name</label>
+          <input v-model='lastname' type="text" class="form-control" id="lastname">
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input v-model="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input v-model="password" type="password" class="form-control" id="password">
+      </div>
+      <div class="text-center mb-5">
+        <router-link to="login" class="text-decoration-none">Log In</router-link>
+      </div>
+
+      <div class="text-center">
+        <button @click="register" class="btn btn-primary">Register</button>
+      </div>
+    </form>
+  </div>
+
 </template>
 
 <style scoped>
