1ed6e384c6db789cd985be883f11b11a87141abd
alyaz009 <yazmin.cedillo01@gmail.com>
2024-03-12 13:30:38 -0500

    form added in create project

--- HobbyPro/src/components/CreateProject.vue
+++ HobbyPro/src/components/CreateProject.vue
@@ -7,6 +7,19 @@
     <h1>create project page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, laborum qui obcaecati blanditiis cumque sapiente distinctio animi ad neque fuga sit suscipit? Commodi, minus id ullam aperiam similique ad vero!</p>
   </div>
+  <form name="createProject">
+        <div>
+            <div class="inputElement">
+                <label for="projectName">Project Name: </label>
+                <input type="text" name="projectName" id="projectName">
+            </div>
+            <div class="inputElement">
+                <label for="startDate">Start Date: </label>
+                <input type="date" name="startDate" id="startDate">
+                
+            </div>
+        </div>
+    </form>
   
 </template>
 
