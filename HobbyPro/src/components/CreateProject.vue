0e02f49a6c9f772535d04c5348530d67f45455e6
alyaz009 <yazmin.cedillo01@gmail.com>
2024-03-12 13:24:11 -0500

    edited create project

--- HobbyPro/src/components/CreateProject.vue
+++ HobbyPro/src/components/CreateProject.vue
@@ -7,13 +7,12 @@
     <h1>create project page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, laborum qui obcaecati blanditiis cumque sapiente distinctio animi ad neque fuga sit suscipit? Commodi, minus id ullam aperiam similique ad vero!</p>
   </div>
+  
 </template>
 
 <style scoped>
 @media (min-width: 576px) {
   .CP_formatting {
-    min-height: 100vh;
-    display: flex;
     align-items: center;
   }
 }
