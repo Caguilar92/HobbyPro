e52d0b8bb0dda378ea389b6eb5de4f01156d0705
alyaz009 <yazmin.cedillo01@gmail.com>
2024-03-14 16:37:40 -0500

    comments in main.js

--- HobbyPro/src/main.js
+++ HobbyPro/src/main.js
@@ -6,14 +6,14 @@ import {router} from "@/router/index.js";
 import App from "@/App.vue";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import Vue from 'vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
 
 
 
