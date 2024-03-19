45455f3161cc5705c03dc898a90d835de955d119
Candelario <candelarioa42@gmail.com>
2024-03-19 13:49:35 -0500

    "added sign out to profile dropdown menu"

--- HobbyPro/src/components/NavigationBar.vue
+++ HobbyPro/src/components/NavigationBar.vue
@@ -1,10 +1,16 @@
 <script>
+import {getAuth,signOut} from "firebase/auth";
+import {useRouter} from "vue-router";
+
 export default {
   data() {
     return {
       // State to track if the dropdown is visible
       isRouterDropdownVisible: false,
       isProfileDropdownVisible: false,
+      auth: getAuth(),
+      router: useRouter()
+
     };
   },
   methods: {
@@ -15,6 +21,16 @@ export default {
     toggleProfileDropdown() {
       this.isProfileDropdownVisible = !this.isProfileDropdownVisible;
     },
+
+    log_out(event) {
+      event.preventDefault();
+
+      signOut(this.auth).then(() => {
+        this.router.replace('/login');
+      }).catch((error) => {
+        console.log("something went wrong")
+      });
+    }
   },
 };
 </script>
@@ -44,6 +60,10 @@ export default {
         <li><RouterLink to="/dashboard/profile">Profile</RouterLink></li>
         <li><RouterLink to="/dashboard/profile"><div>Profile</div></RouterLink></li>
       </ul>
+
+      <div class="MP_formatting">
+        <button @click="log_out" class="btn btn-primary">sign out</button>
+      </div>
     </div>
   </header>
 
