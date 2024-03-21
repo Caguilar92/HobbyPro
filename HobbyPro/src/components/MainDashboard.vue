7a6ebf547464785142db6dcce7a68acf5c467c9e
Samuel Fletcher <sfletcher6@my.apsu.edu>
2024-03-21 13:48:18 -0500

    main dashboard dummy data

--- HobbyPro/src/components/MainDashboard.vue
+++ HobbyPro/src/components/MainDashboard.vue
@@ -3,6 +3,13 @@
 import {getAuth,signOut} from "firebase/auth";
 import {useRouter} from "vue-router";
 
+//tester for list of projects
+const projects = [
+  {title: 'ninja UX Designer', id: 1, details: 'lorem', deadline: "10/24/2024" },
+  {title: 'ninja Web Developer', id: 2, details: 'lorem', deadline: "11/27/2030"},
+  {title: 'ninja Vue Developer', id: 3, details: 'lorem', deadline: "05/18/2027" },
+]
+
 
 const auth = getAuth();
 const router = useRouter();
@@ -20,6 +27,14 @@ function log_out(event) {
   <div class="MP_formatting">
     <h1>Main Dashboard</h1>
     <button @click="log_out" class="btn btn-primary">sign out</button>
+    <div>
+      <ul class="project-list" v-for="project in projects" :key="project.id">
+        <li>
+            <h3>{{ project.title }}</h3>
+            <p>deadline: {{ project.deadline }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
