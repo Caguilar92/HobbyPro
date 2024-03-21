7471994ed74acb4a8fa87cab7f3d4b0492b554a9
d-p-bailey <dpb.bailey@gmail.com>
2024-03-21 13:54:37 -0500

    working in completed projects still trying to get storage working

--- HobbyPro/src/components/CompletedProject.vue
+++ HobbyPro/src/components/CompletedProject.vue
@@ -1,19 +1,31 @@
 <script setup>
+  import { getAuth } from "firebase/auth";
   import {ref} from "vue";
   const email = ref('uNITIAL DATA');
-  function changeEmail(){
-    email.value = "this works!";
+  
+  
+  // gets user info from auth 
+  const auth = getAuth();
+  const user = auth.currentUser()
+  const userEmail = user.email;
+  
+  function loadDocument(file){
+    // 'file' comes from the Blob or File API
+    // file is saved to a sepperate folder based on user email address
+    uploadBytes(storageRef, userEmail+"/"+fileName.value).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+    });
   }
 
-
   
-
+  
   
 </script>
 <template>
     <div class="completed">
         <h1>this is the completed projects page</h1>
-        <button @click = "changeEmail">{{email}}</button>
+        <input type="file" name="fileName" id="fileName" accept=".png, .jpeg, .gif"> 
+        <button @click = "changeEmail">TEST BUTTON</button>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, laborum qui obcaecati blanditiis cumque sapiente distinctio animi ad neque fuga sit suscipit? Commodi, minus id ullam aperiam similique ad vero!</p>
     </div>
 </template>
