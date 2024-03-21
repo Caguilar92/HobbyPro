500f2957dcc90fbd9f8bcbec07b6b310e791fae4
alyaz009 <yazmin.cedillo01@gmail.com>
2024-03-21 16:22:16 -0500

    edited library

--- HobbyPro/src/components/Library.vue
+++ HobbyPro/src/components/Library.vue
@@ -3,10 +3,19 @@
 </script>
 
 <template>
-<div class="library_formatting">
-    <h1>Library</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptas architecto inventore harum tempora deleniti enim, ducimus dignissimos facere, repudiandae sed amet temporibus similique modi accusamus cum blanditiis repellat magni?</p>
-</div>
+    <div class="library_formatting">
+        <h1>Library</h1>
+        <p>example card files</p>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="" alt="image" />
+            <div class="card-body">
+                <h4 class="card-title">Sample file name</h4>
+
+                <p class="card-text">Text</p>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <style>
