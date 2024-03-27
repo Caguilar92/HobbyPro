ee90c3810c9a7915ce176cba3c3918d54a93c2b2
Candelario <candelarioa42@gmail.com>
2024-03-26 19:11:20 -0500

    "added  examples to upload and retrieve and display data"

--- HobbyPro/src/components/Library.vue
+++ HobbyPro/src/components/Library.vue
@@ -1,6 +1,65 @@
-<script>
+<script setup>
+import {onMounted, ref} from "vue";
+import {getStorage,ref as sRef,uploadBytes,listAll,getDownloadURL} from "firebase/storage";
+import {findShadowRoot} from "bootstrap/js/src/util/index.js";
+
+let selectedFile = ref(null);
+let fileInput = ref(null)
+const storage = getStorage();//storage root reference
+let imageUrls= ref([])
+
+function uploadFile(e) {
+
+  e.preventDefault();
+  if(selectedFile.value != null) {//ensure file is not null
+    const storageRef = sRef(storage,"images/" + selectedFile.value.name)//path to where file is to be stored in this case images/spider-man.png
+    uploadBytes(storageRef,selectedFile.value).then((snapshot)=> {//actual function to upload files
+      console.log("UPLOADED FILE Successfully")
+    }).catch((error)=> {
+      console.log("error: something wen wrong")//handle  any errors here
+    })
+  }
+
+}
+function handleFile(event) {
+  const file = event.target.files[0];
+  const name = event.target.files[0].name;
 
 
+  if(file){
+    selectedFile.value = file;//get file selected
+    const fileFormat = name.split('.')[1];//gets extension
+    if(fileFormat !== 'jpeg' && fileFormat !== 'jpg' && fileFormat !== 'png'){//check extension to ensure it meets requirements
+                                                                              //if wrong file extension clear input and null out file
+      console.log("ERROR: MUST BE AN IMAGE type")
+      fileInput.value = null;
+      selectedFile.value = null;
+    }
+
+  }
+
+}
+
+async function getImages() {
+   const storageRef = sRef(storage,"images/")
+   listAll(storageRef)
+      .then((result) => {
+        result.items.map((item) => {
+          getDownloadURL(item)
+              .then((url) => {
+                  imageUrls.value.push(url)
+          });
+
+        })
+      })
+
+
+
+}
+
+onMounted(()=> {
+  getImages()
+})
 </script>
 
 <template>
@@ -25,19 +84,28 @@
         </div>
 
       </div>
-      <main>
-        <p>example card files</p>
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="" alt="image" />
-          <div class="card-body">
-            <h4 class="card-title">Sample file name</h4>
 
-            <p class="card-text">Text</p>
+    <div>
+      <div v-for="(url, index) in imageUrls" :key="index">
+        <div class="card" style="width: 18rem;">
+          <img :src="url" alt="Image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-      </main>
 
+      </div>
     </div>
+
+  </div>
+  <label for="item-image-input">File</label>
+  <input type="file" id="item-file-input" v-bind="fileInput" @change="handleFile">
+
+  <div class="mt-4">
+    <button @click="uploadFile" type="button" class="btn btn-primary">Upload</button>
+  </div>
 </template>
 
 <style>
