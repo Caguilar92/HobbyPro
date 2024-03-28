9e532efaec0c86679637d7808e80367e4239105a
Samuel Fletcher <sfletcher6@my.apsu.edu>
2024-03-27 22:39:18 -0500

    final version NavigationBar

--- HobbyPro/src/main.js
+++ HobbyPro/src/main.js
@@ -1,11 +1,10 @@
-import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import {router} from "@/router/index.js";
 import App from "@/App.vue";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
+//import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
