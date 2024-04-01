01cdef6eb4252a2c301c83e928fbd2427154fc55
alyaz009 <yazmin.cedillo01@gmail.com>
2024-04-01 11:04:30 -0500

    firebase functions

--- HobbyPro/src/firebase_functions.js
+++ HobbyPro/src/firebase_functions.js
@@ -3,10 +3,13 @@ import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 //this is a class to organize the return from the database
 export class Project {
-  constructor(projectName, startDate, deadline){
+  constructor(projectName, uid, startDate = null, deadline = null, description = null){
     this.projectName = projectName;
+    this.uid = uid;
     this.startDate = startDate;
     this.deadline = deadline;
+    this.description = description;
+
   }
 }
 
@@ -34,11 +37,34 @@ export function saveToFirestore(event) {
   setDoc(doc(firestore, "Projects", projectName.value), {
     projectName: projectName.value,
     startDate: startDate.value,
-    deadline: deadline.value
+    deadline: deadline.value,
+    description: description
   }).then(() => {
     console.log('Document written with ID: ', projectName.value);
   }).catch((error) => {
     console.error('Error adding document: ', error);
   });
   console.log("project uploaded");
+}
+
+// Retrieve data from Firestore and populate 'projects' array
+export async function getDocsFromDatabase() {
+  try{
+    const projectsCollectionRef = collection(firestore, 'Projects');
+    const querySnapshot = await getDocs(projectsCollectionRef);
+
+    const projects = [];
+    //fills projects array from firestore
+    querySnapshot.forEach(doc => {
+      const projectData = doc.data();
+      const projectID = doc.id;
+      const projectInstance = new Project(projectData.projectName, projectID, projectData.startDate, projectData.endDate);
+      projects.push(projectInstance);
+    });
+    // Return projects array
+    return projects;
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    return [];
+  }
 }
