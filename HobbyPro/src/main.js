d5a704c1ee455944c7bb438907feeebc6c27e481
Candelario <candelarioa42@gmail.com>
2024-04-13 11:01:05 -0500

    "added boostrap popper to dependency"

--- HobbyPro/src/main.js
+++ HobbyPro/src/main.js
@@ -5,8 +5,8 @@ import { createPinia } from 'pinia'
 import {router} from "@/router/index.js";
 import App from "@/App.vue";
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'popper.js/dist/umd/popper.min.js'
 
 
 const app = createApp(App)
