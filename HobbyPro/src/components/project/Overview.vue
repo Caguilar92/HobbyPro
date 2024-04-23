3c8179fd74e0c016ca399582f3167ab6bd39dc37
Samuel Fletcher <sfletcher6@my.apsu.edu>
2024-04-23 13:46:57 -0500

    overview days left method changed to reflect changes in main

--- HobbyPro/src/components/project/Overview.vue
+++ HobbyPro/src/components/project/Overview.vue
@@ -21,12 +21,27 @@ onMounted(async() => {
   project.value = store.state.selectedProject;
 });
 
-const daysLeft = (deadline) => {
+const daysLeft = (deadline, startdate) => {
+  if(deadline == "") {
+    return "Infinite";
+  }
   const today = new Date();
   const deadlineDate = new Date(deadline);
-  const timeDifference = deadlineDate.getTime() - today.getTime();
-  const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
-  return daysDifference >= 0 ? daysDifference : 0;
+  const beginingDate = new Date(startdate);
+  const startTimeDifference = beginingDate.getTime() - today.getTime();
+  const startDateDifference = Math.ceil(startTimeDifference / (1000 * 3600 * 24));
+  //console.log("check start date difference: " + startDateDifference);
+  if (startDateDifference >= 0) {
+    const timeDifference = deadlineDate.getTime() - beginingDate.getTime();
+    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    //console.log("used startdate - deadline: " + daysDifference);
+    return daysDifference >= 0 ? daysDifference : 0;
+  } else {
+    const timeDifference = deadlineDate.getTime() - today.getTime();
+    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    //console.log("used today - deadline: " + daysDifference);
+    return daysDifference >= 0 ? daysDifference : 0;
+  }
 };
 </script>
 
@@ -50,7 +65,8 @@ const daysLeft = (deadline) => {
               <p>Start Date: {{ project.startDate }}</p>
             </div>
             <div class="col-sm-7">
-              <p>Deadline: {{ project.deadline }} Days Left: {{ daysLeft(project.deadline) }}</p>
+              <p v-if="project.deadline === ''">No Deadline</p>
+              <p v-else>Deadline: {{ project.deadline }}  Days Left: {{ daysLeft(project.deadline, project.startDate) }}</p>
             </div>
           </div>
 
